58ae4f470efc64911285383beea886a39454d5c4
hassanamin994 <hassanmamin994@gmail.com>
2020-04-28 22:26:08 +0200

    enable whatsapp bot only when availabnle

--- router/index.js
+++ router/index.js
@@ -30,7 +30,9 @@ module.exports = (app) => {
 
     return next();
   });
-  app.use('/api/whatsapp-webhook', createProxyRouter(process.env.WHATSAPP_BOT_API_ROOT))
+  if (process.env.WHATSAPP_BOT_API_ROOT) {
+    app.use('/api/whatsapp-webhook', createProxyRouter(process.env.WHATSAPP_BOT_API_ROOT))
+  }
 
   app.use('/api/auth', createProxyRouter(process.env.AUTH_SERVICE_API_ROOT));
 
